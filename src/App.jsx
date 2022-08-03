772bea100a764ef925d0b3920af6983039b01595
Olstertecn11 <otzunund@miumg.edu.gt>
2022-08-03 11:38:21 -0600

    first complete version from principal page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
 import './App.css';
 import Nav from './components/Nav';
+import img from './components/imgs/undraw_gardening_re_e658.svg';
 
 function App() {
   return (
     <>
       <Nav />
-      <p className="title">
-	Your landscape cared for the way they should be
-	by 
-      <b className="name">Rodriguez Landscaping</b>
-      </p>
+      <div className="container title-container">
+	<div className="row">
+	  <div className="col">
+	    <p className="title">
+	      Your landscape cared for the way they should be
+	      by 
+	      <b className="name">Rodriguez Landscaping</b>
+	    </p>
+	  </div>
+	  <div className="col">
+	  </div>
+	</div>
+      </div>
+      
+
+      <div className="container">
+      	<div className="row">
+	  <div className="col">
+	    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis atque animi quos assumenda nulla cum maxime suscipit quae veritatis, voluptas unde tempora distinctio adipisci dolores alias molestias laborum culpa nobis!</p>
+	    <a href="#" className="btn btn-success text-white">Read More</a>
+	  </div>
+	  <div className="col">
+	    <img className="img" src={img} width={200} alt=""/>
+	  </div>
+	</div>
+      </div>
+      
       <div className="circle">
       </div>
     </>
