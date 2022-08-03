0e1144f15c353a986159cbf54cf61ad2eae093f2
Olstertecn11 <otzunund@miumg.edu.gt>
2022-08-02 23:19:14 -0600

    first version from principal page

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p className="title">
+	Your landscape cared for the way they should be
+	by 
+      </p>
+      <b className="name">Rodriguez Landscaping</b>
+      <div className="circle">
+      </div>
+    </>
+  );
+}
+
+export default App;
