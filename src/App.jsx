ed38759ac5e727e8b27324bd78678da7bc6a27ae
Olivers11 <olstertecn597@gmail.com>
2022-08-09 18:53:32 -0600

    fixing responsive design xdxdxd

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 	  <p>
 Our lawn and landscape maintenance services are customizable to your needs including: lawn mowing, landscape maintenance, shrub trimming, bed weeding, property clean ups, and leaf removal services. We strive to provide the best lawn care in. Maryland, Virginia and washington If you have a property needing lawn service please contact us.
 	  </p>
-	    <a href="#" className="btn btn-success text-white">Read More</a>
+	    <a href="#" className="btn btn-read text-white">Read More</a>
 	  </div>
 	  <div className="col">
 	    <img className="img" src={img} width={200} alt=""/>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,15 +4,26 @@ import img from './imgs/logo.jpeg';
 const Nav = ()=>{
   return(
     <>
-      <nav class="navbar">
-	<div class="container-fluid">
-	  <a class="navbar-brand"> <img src={img} alt="" className="logo" /> </a>
-	  <form class="mx-right buttons-form">
-	    <button class="btn btn-outline-success" type="submit">Home</button>
-	    <button class="btn btn-outline-success" type="submit">Gallery</button>
-	    <button class="btn btn-outline-success" type="submit">Services</button>
-	    <button class="btn btn-outline-success" type="submit">Contact</button>
-	  </form>
+      <nav className="navbar navbar-expand-lg">
+	<a className="navbar-brand" href="#"><img src={img} className='logo'/>  Landscaping</a>
+	<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+	  <span className="navbar-toggler-icon"></span>
+	</button>
+	<div className="collapse navbar-collapse" id="navbarText">
+	  <ul className="navbar-nav ml-auto">
+	    <li className="nav-item active">
+	      <a className="nav-link" href="#">Home</a>
+	    </li>
+	    <li className="nav-item">
+	      <a className="nav-link" href="#">Services</a>
+	    </li>
+	    <li className="nav-item">
+	      <a className="nav-link" href="#">Gallery</a>
+	    </li>
+	    <li className="nav-item">
+	      <a className="nav-link" href="#">Contact</a>
+	    </li>
+	  </ul>
 	</div>
       </nav>
     </>
